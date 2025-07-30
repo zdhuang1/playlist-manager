a132d27418aa4e4d93889563c1d32638197f8de4
Zachary Huang <zdhuang@ucsd.edu>
2025-07-30 13:08:18 -0700

    Implement electron for app desktop version and update dependencies

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
 // To run backend: node server.js
-// To start React frontend: npm start (from the playlist-manager dir)
+// Run the following from the 'playlist-manager' dir
+//    - To launch React frontend (browser version): npm start
+//    - To launch React frontend (desktop version): npm run electron 
+
+/*
+ * Don't worry about dev packages bc they wont be shipped.
+ * 
+ * Good practice is to periodically run: 
+ *    npm outdated                # see what you can bump
+ *    npx npm-check-updates -u    # update ranges in package.json
+ *    npm install 
+ * This shows outdated packages and allows you to update dependencies
+ * 
+ * If package vulnerabilites are stuck, try:
+ *    rm -rf node_modules package-lock.json
+ *    npm install
+ */
 
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect } from 'react';
+import { getTopTracks } from './spotifyApi';
+
+const topTracks = await getTopTracks();
+console.log(
+  topTracks?.map(
+    ({name, artists}) =>
+      `${name} by ${artists.map(artist => artist.name).join(', ')}`
+  )
+);
+
+
 function App() {
+  useEffect(() => {
+    async function fetchTracks() {
+      const topTracks = await getTopTracks();
+      console.log(
+        topTracks?.map(
+          ({ name, artists }) =>
+            `${name} by ${artists.map(artist => artist.name).join(', ')}`
+        )
+      );
+    }
+    fetchTracks();
+  }, []);
+  
   return (
-    <div className="App">
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Pick a song
+          </p>
+
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
-        </a>
+          </a>
       </header>
-    </div>
+      </div>
   );
 }
 
---
+++ src/spotifyApi.js
@@ -0,0 +1,20 @@
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+const token = 'undefined'; // Replace with your actual token for testing
+
+export async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body: JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+export async function getTopTracks() {
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+  )).items;
+}
