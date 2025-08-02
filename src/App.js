b90fdc23a137e2566bcae1a9e9db0541913c14be
Zachary Huang <zdhuang@ucsd.edu>
2025-08-01 22:34:43 -0700

    Move CSS file to align with per-component/page styling structure

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Callback from './pages/Callback';
-import './App.css';
+
 
 function App() {
   return (
